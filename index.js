b1413b723ebea1807c425174c6ba41e971b1b727
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2019-11-19 18:38:13 +0300

    fix: add features

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ function generateKeys() {
 	const arrResult = [];
 	const arraySymbols = symbols.split('');
 	
-	for (let i = 0; i < 15; i++) {
+	for (let i = 0; i < 10; i++) {
 		arrResult.push(arraySymbols[Math.floor(Math.random() * arraySymbols.length)])
 	}
 	
 	const key = arrResult.join('');
-	document.querySelector('.resultKeys').innerHTML = `<span style="color: tomato">Key</span>: ${key}`;
+	document.querySelector('.resultKeys').innerHTML = `<span style="color: tomato; font-weight: bold">KEY</span>: ${key}`;
+	
+	let reg = /\$\d+/;
+	if (key.match(reg)) {
+		const win = key.match(reg)
+		alert(`УРА!! вы выиграли ${win}`)
+	}
 }
 
 document.getElementById('btn').addEventListener('click', generateKeys);
