fd727343fae32aa0f77b9c1568b5b5608a5b24e8
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2021-01-15 14:28:03 +0300

    Commit |

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 'use strict';
 
+let count = 0;
 
 function generateKeys() {
-	const symbols = `1!2@3#4$5%67&89_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM`;
+	count++;
+
+	const symbols = `1!2@3#4$5'67&89qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM`;
 	const arrResult = [];
 	const arraySymbols = symbols.split('');
-	
-	for (let i = 0; i < 10; i++) {
-		arrResult.push(arraySymbols[Math.floor(Math.random() * arraySymbols.length)])
+
+	for (let i = 0; i < 16; i++) {
+		if (i === 5 || i === 10) {
+			arrResult.push('_')
+		} else {
+			arrResult.push(arraySymbols[Math.floor(Math.random() * arraySymbols.length)])
+		}
 	}
-	
+
 	const key = arrResult.join('');
 	document.querySelector('.resultKeys').innerHTML = `<span style="color: tomato; font-weight: bold">KEY</span>: ${key}`;
-	
+
 	let reg = /\$\d+/;
 	if (key.match(reg)) {
-		const win = key.match(reg)
+		const win = key.match(reg);
 		alert(`УРА!! вы выиграли ${win}`)
 	}
+	localStorage.setItem(`key_${count}`, key) // save in localStorage
 }
 
 document.getElementById('btn').addEventListener('click', generateKeys);
